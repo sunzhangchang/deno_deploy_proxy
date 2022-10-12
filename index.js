b86257a4495f3d6c8d9dbce52db430d60918326f
sunzhangchang <642311409@qq.com>
2022-10-12 21:15:27 +0800

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 addEventListener('fetch', e => {
     const url = new URL(e.request.url)
     url.hostname = 'gist.github.com'
+    console.log(url)
     const request = new Request(url, event.request);
     e.respondWith(fetch(request))
 })
