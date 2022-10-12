2a68b7278317905fd494751e85a62bae27f52740
sunzhangchang <642311409@qq.com>
2022-10-12 21:16:01 +0800

    fix: :bug: fix event

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ addEventListener('fetch', e => {
     const url = new URL(e.request.url)
     url.hostname = 'gist.github.com'
     console.log(url)
-    const request = new Request(url, event.request);
+    const request = new Request(url, e.request);
     e.respondWith(fetch(request))
 })
