da9f4ad34a6ed1ce958414263a171b8eb2e39a28
sunzhangchang <642311409@qq.com>
2022-10-13 08:31:09 +0800

    feat: add support for custom domains

--- index.ts
+++ index.ts
@@ -2,10 +2,24 @@ import { serve } from 'https://deno.land/std@0.155.0/http/server.ts'
 
 // serve((req: Request) => new Response("Hello World"))
 
-const proxy_domain = 'https://gist.github.com' // 修改为你的网站地址
+// const proxy_domain = 'https://gist.github.com'
 
 function handler(req: Request): Promise<Response> {
-    const url = proxy_domain + req.url.split('deno.dev')[1]
+    const parts = req.url.split('deno.dev')
+    let url = ''
+
+    if (parts.length > 1) {
+        parts.shift()
+        url = parts.join('deno.dev')
+    }
+
+    while (url.startsWith('/')) {
+        url = url.substring(1)
+    }
+
+    if (url.length === 0) {
+        url = 'https://gist.github.com'
+    }
 
     return fetch(url, {
         headers: req.headers,
